e87c6f67deae08fac701ad8b623e36fb282bb0a4
auauauau <auauauau@gmail.com>
2025-03-24 22:33:21 -0300

    Iniciado exercicio 8 da lista 4

---
+++ Lista04/Ex08/Program.cs
@@ -0,0 +1,28 @@
+﻿//8. (Contagem de vogais) Implemente um programa que conte o
+//número de cada uma das vogais em uma frase informada pelo usuário.
+//Ordem dos dados de entrada: Frase
+
+int contadorDeVogais = 0;
+
+char[] vogais = new char[5];
+
+vogais[0] = 'a';
+vogais[1] = 'e';
+vogais[2] = 'i';
+vogais[3] = 'o';
+vogais[4] = 'u';
+
+string frase = Console.ReadLine();
+
+for(int indexLetra = 0; indexLetra < frase.Length; indexLetra++)
+{
+    foreach(char vogal in vogais)
+    {
+        if(vogal == frase[indexLetra])
+        {
+            contadorDeVogais++;
+        }
+    }
+}
+
+Console.WriteLine($"Na frase há {contadorDeVogais} vogais");
--- Lista04/Ex08/obj/Debug/net8.0/Ex08.AssemblyInfo.cs
+++ Lista04/Ex08/obj/Debug/net8.0/Ex08.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ex04")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ex08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a044c23d2d783cb66ea8e365c48a082cae65dc5")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ex04")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ex04")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ex08")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ex08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
